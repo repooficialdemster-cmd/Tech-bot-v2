2047f07699d4d7f26365d9f267092fc82ddcbf48
repooficialdemster-cmd <repooficialdemster@gmail.com>
2025-12-01 13:48:14 -0400

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -4,13 +4,15 @@ var handler = async (m, { conn }) => {
     const contacto = `
 🤖 *¿QUIERES CONTACTAR A MI CREADOR?* 🤖
 
-📞 *Número:* #1 +5491151545427
-📞 *Numero:* #2 +5492644893953
-🔔 *tareas* nodé.js Python.js
+👤 *nombre* *Ghostdev*
+📞 *Número:*#1 5491151545427
+👤 *nombre* *WILKER OFC*
+📞 *Numero:*#2 5492644893953
+🔔 *Lenguajes* nodé.js Python.py
 
 💫 *NOTA IMPORTANTE:*
 
-¿Tienes preguntas, dudas o sugerencias sobre el funcionamiento de *Chrome Bot*? Puedes contactar a mi creador.
+¿Tienes preguntas, dudas o sugerencias sobre el funcionamiento de *tech bot*? Puedes contactar a mi creador.
 
 *Una cosa:* Los bots no descansan, pero yo sí, así que no me andes mandando mensaje a las 3am porque no te voy a contestar… 😴
 
